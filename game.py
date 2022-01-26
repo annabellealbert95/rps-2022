c64894ee72f58ed5b4769c2b4d301f7ff2f27ec2
annabellealbert95 <80716097+annabellealbert95@users.noreply.github.com>
2022-01-26 18:19:59 -0500

    Update game.py
    started the exercise in class

--- game.py
+++ game.py
@@ -5,3 +5,31 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+# objectives to tackle: 
+
+# Ask for a user input (asking rock, paper, or scissors)
+u = input("rock,paper,scissors")
+print(u)
+
+
+
+
+# validate user input
+# computer choice 
+# determine the winner
+# display the final results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
